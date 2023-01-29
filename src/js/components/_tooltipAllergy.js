184919a62341c70c9ceec65000a6cc61b45b54b4
opect.pol@i.ua <opect.pol@i.ua>
2023-01-29 23:24:16 +0200

    fix tooltip

--- src/js/components/_tooltipAllergy.js
+++ src/js/components/_tooltipAllergy.js
@@ -2,10 +2,19 @@ new jBox('Tooltip', {
   attach: '[tip]',
   preventDefault: true,
   trigger: 'click',
-  animation: 'slide',
+  animation: 'move',
+  width: 450,
   closeOnClick: false,
   closeButton: true,
   maxWidth: 450,
+  adjustTracker: true,
+  closeOnEsc: true,
+  position: {
+    x: 'right',
+    y: 'center',
+  },
+  outside: 'x',
+  pointer: 'left:50',
   onOpen: function () {
     this.source[0].classList.add('scale');
     this.setContent($('.tooltip-' + this.source.attr('tip')));
